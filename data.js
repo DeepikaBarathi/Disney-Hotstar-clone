97cf7cac426ee8d4d3f960198bc06747ff47b4f7
DeepikaBarathi <159872250+DeepikaBarathi@users.noreply.github.com>
2024-04-13 16:55:31 +0530

    Add files via upload

---
+++ data.js
@@ -0,0 +1,22 @@
+let movies = [
+    {
+        name: 'LUCA',
+        des: '1hr 23min,English,2021.Luca, a sea monster, befriends Alberto, another one of his kind who takes him on a land adventure. He experiences an exciting summer while keeping his parents in the dark.!',
+        image: 'img/slider 5.PNG'
+    },
+    {
+        name: 'wanda vision',
+        des: 'Vision and Wanda live a normal life in Westview and conceal their superpowers. However, as decades pass by, they start doubting that everything is not what it seems.!',
+        image: 'img/slider 3.PNG'
+    },
+    {
+        name: 'raya and the last dragon',
+        des: 'Raya, a warrior, sets out to track down Sisu, a dragon, who transferred all her powers into a magical gem which is now scattered all over the kingdom of Kumandra, dividing its people.!',
+        image: 'img/slider 4.PNG'
+    },
+    {
+        name: 'LOKI',
+        des: 'After stealing the Tesseract, Loki comes into contact with a mysterious organization that gives him an ominous ultimatum, either fix the timeline or cease to exist completely.!',
+        image: 'img/slider .PNG'
+    }
+]
